f8ed6d768ff04f036ff288fed6e6ebab50189da0
pasinduyy <pasinduyy@gmail.com>
2026-02-14 18:14:43 +0530

    reuse shared scanner instance

--- src/main/java/io/github/pasinduyy/Console.java
+++ src/main/java/io/github/pasinduyy/Console.java
@@ -3,8 +3,13 @@ package io.github.pasinduyy;
 import java.util.Scanner;
 
 public class Console {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static double readNumber(String prompt) {
+        return scanner.nextDouble();
+    }
+
     public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
         double value;
 
         while (true) {
